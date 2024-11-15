98fbc20f408231f5194bab229716c81f19e4343e
brunocmartins11 <brunocmartins11@gmail.com>
2024-11-15 15:12:46 -0300

    Correção do modelo Atendimento

--- models/atendimento.js
+++ models/atendimento.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const atendimentoSchema = new mongoose.Schema({
   titulo: String,
-  tipo, String,
+  tipo: String,
   diagnostico, String,
   corpo: String,
   pacienteId: { type: mongoose.Schema.Types.ObjectId, ref: 'Paciente' } 
