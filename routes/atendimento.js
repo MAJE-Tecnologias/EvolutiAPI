27dce8a1edbcba75f33b4445b9b63d83cb7d2ba2
brunocmartins11 <brunocmartins11@gmail.com>
2024-11-15 15:09:50 -0300

    Correação de gunção no Atendimento

--- routes/atendimento.js
+++ routes/atendimento.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { buscarAtendimento, criarAtendimento, excluirAtendimento, editaratendimento} from '../controllers/atendimentoController.js';
+import { buscarAtendimento, criarAtendimento, excluirAtendimento, editarAtendimento} from '../controllers/atendimentoController.js';
 
 const router = express.Router();
 
 router.post('/clinicas', criarAtendimento);
 router.get('/clinicas', buscarAtendimento);
 router.delete('/clinicas/id=:id', excluirAtendimento);
-router.put('/clinicas/id=:id', editaratendimento);
+router.put('/clinicas/id=:id', editarAtendimento);
 
 export default router;
